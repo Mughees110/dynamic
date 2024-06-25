cfb711c131076e68e5388295364323dd7a0dd418
Mughees110 <zaidimughees@gmail.com>
2024-06-26 01:48:26 +0500

    ds

--- database/migrations/2024_06_25_195743_create_reports_table.php
+++ database/migrations/2024_06_25_195743_create_reports_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('result')->nullable();
             $table->string('value')->nullable();
+            $table->string('clientId')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2024_06_25_195843_create_parameters_table.php
+++ database/migrations/2024_06_25_195843_create_parameters_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->string('value')->nullable();
+            $table->string('clientId')->nullable();
             $table->timestamps();
         });
     }
